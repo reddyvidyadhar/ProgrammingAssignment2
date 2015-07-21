e5466e2d523a2cc848c93a9000b01d7bd94a1f20
Vidyadhar Reddy <vidyadhar.reddy23@gmail.com>
2015-07-21 19:32:07 -0400

    reusing previous version
    updated comments spacing

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,6 @@
 # The first function, makeCacheMatrix creates a special "matrix", 
-# which is really a list containing a function to
-# set the value of the matrix
-# get the value of the matrix
-# set the value of the inverse
+# which is really a list containing a function to set the value of the matrix
+# get the value of the matrix set the value of the inverse
 # get the value of the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
@@ -30,13 +28,10 @@ makeCacheMatrix <- function(x = matrix()) {
     
 }
 
-## The following function calculates the inverse of the special 
-## "matrix" created with the above function. 
-## It first checks to see if the inverse has already been calculated. 
-## If so, it gets the inverse from the cache and skips the 
-## computation. Otherwise, it calculates the inverse of the 
-## matrix and sets the value of the inverse in the cache via 
-## the setinv function.
+# The following function calculates the inverse of the special "matrix" created with the above function. 
+# It first checks to see if the inverse has already been calculated. If so, it gets the inverse from the cache and skips the 
+# computation. Otherwise, it calculates the inverse of the matrix and sets the value of the inverse in the cache via 
+# the setinv function.
 
 cacheSolve <- function(x, ...) {
     # check if the inverse is already cached,
